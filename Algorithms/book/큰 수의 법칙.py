cf578dc94022707c3309c8017a4c74781cf1b7c5
Chan Henry <48753593+o-henry@users.noreply.github.com>
2020-10-22 12:48:57 +0900

    Create 큰 수의 법칙.py

---
+++ Algorithms/book/큰 수의 법칙.py
@@ -0,0 +1,23 @@
+```py
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+# 2가지 조합으로 해결 가능
+
+sum = 0
+cnt = 1
+
+data.sort()
+operand_one = data[-1]
+operand_two = data[-2]
+
+while cnt <= m:
+  if cnt%k == 0:
+    print(sum)
+    sum += operand_two
+  else:
+    sum += operand_one   
+    print(sum)
+  cnt+=1
+
+print(sum)
+```
