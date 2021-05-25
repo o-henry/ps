3ef621e3b7ccea72e2425259632f48894981c612
henry <cksgod16@gmail.com>
2021-05-25 23:14:00 +0900

    Update sum-of-digits

--- Algorithms/inflearn/javascript/sum-of-digits.js
+++ Algorithms/inflearn/javascript/sum-of-digits.js
@@ -1,8 +1,26 @@
 function solution(list) {
-  let answer;
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+
+  for (let x of list) {
+    let sum = 0,
+      tmp = x;
+
+    while (tmp) {
+      sum += tmp % 10;
+      tmp = Math.floor(tmp / 10);
+    }
+
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
 
   return answer;
 }
 
 let arr = [128, 460, 603, 40, 521, 137, 123];
-console.log(solution(7, arr));
+console.log(solution(arr));
