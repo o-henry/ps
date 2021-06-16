c9fbe10a7f5f438a24a35eae0c25be3b4ee6e077
henry <cksgod16@gmail.com>
2021-06-16 23:26:25 +0900

    Add two-pointer

---
+++ Algorithms/inflearn/javascript/two-pointer-algorithm.js
@@ -0,0 +1,22 @@
+function twoPointer(a, b) {
+  let answer = [];
+  a.sort();
+  b.sort();
+  let pa = 0;
+  let pb = 0;
+
+  while (pa < a.length && pb < b.length) {
+    if (a[pa] === b[pb]) {
+      answer.push(a[pa++]);
+      pb++;
+    } else if (a[pa] > b[pb]) pb++;
+    else pa++;
+  }
+
+  return answer;
+}
+
+let a = [1, 3, 9, 5, 2];
+let b = [3, 2, 5, 7, 8];
+
+console.log(twoPointer(a, b));
