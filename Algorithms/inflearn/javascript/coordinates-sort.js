14723e26f5a82f9999a12089d8f901cbff677130
henry <cksgod16@gmail.com>
2021-07-07 17:32:41 +0900

    Add DFS - with stack-frame

--- Algorithms/inflearn/javascript/coordinates-sort.js
+++ Algorithms/inflearn/javascript/coordinates-sort.js
@@ -1,5 +1,10 @@
-function solution() {
-  let answer;
+function solution(arr) {
+  let answer = arr;
+
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    return a[0] - b[0];
+  });
 
   return answer;
 }
---
+++ Algorithms/inflearn/javascript/music-video.js
@@ -0,0 +1,4 @@
+function solution(t, arr) {}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(solution(3, arr));
