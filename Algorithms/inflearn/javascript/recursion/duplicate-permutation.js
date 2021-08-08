82319b87d80aa96cb5c995f95a4924304841dcaf
henry <cksgod16@gmail.com>
2021-08-08 16:07:01 +0900

    Update

--- Algorithms/inflearn/javascript/recursion/duplicate-permutation.js
+++ Algorithms/inflearn/javascript/recursion/duplicate-permutation.js
@@ -1,3 +1,21 @@
-function DuplicatePermutation(n, m) {}
+function DuplicatePermutation(n, m) {
+  let answer = [];
+  let tmp = Array.from({ length: m }, () => 0);
+
+  function DFS(L) {
+    if (L === m) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 1; i <= n; i++) {
+        tmp[L] = i;
+        DFS(L + 1);
+      }
+    }
+  }
+
+  DFS(0);
+
+  return answer;
+}
 
 console.log(DuplicatePermutation(3, 2));
