7f58f44a72ce8d5b612349955bc2c7496e50957c
henry <cksgod16@gmail.com>
2021-08-15 21:26:00 +0900

    Update

---
+++ Algorithms/programmers/distancing.js
@@ -0,0 +1,10 @@
+function solution(places) {}
+
+let p = [
+  ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
+  ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
+  ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
+  ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
+  ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"],
+];
+console.log(solution(p)); // [1,0,1,1,1]
