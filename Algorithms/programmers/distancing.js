8f584fb073920f9c0d229f4df3154dd4de7847ff
henry <cksgod16@gmail.com>
2021-08-16 11:25:28 +0900

    Update

--- Algorithms/programmers/distancing.js
+++ Algorithms/programmers/distancing.js
@@ -1,4 +1,18 @@
-function solution(places) {}
+function solution(places) {
+  let dx = [1, 2, 0, 0];
+  let dy = [0, 0, 1, 2];
+  let d = ["R", "R2", "D", "D2"];
+
+  for (let i = 0; i < places.length; i++) {
+    for (let j = 0; j < places[i].length; j++) {
+      if (palces[i][j] === "P") {
+      }
+    }
+  }
+
+  function DFS(L) {}
+  DFS();
+}
 
 let p = [
   ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
