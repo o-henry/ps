bc9f6bfbcabfe4da01e68469aef9c58c10d28ce4
henry <cksgod16@gmail.com>
2021-08-16 16:11:08 +0900

    Update

---
+++ Algorithms/programmers/kth-num.js
@@ -0,0 +1,24 @@
+function solution(array, commands) {
+  var answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    let list = array.slice(commands[i][0] - 1, commands[i][1]).sort((a, b) => {
+      return a - b;
+    });
+
+    answer.push(list[commands[i][2] - 1]);
+  }
+
+  return answer;
+}
+
+console.log(
+  solution(
+    [1, 5, 2, 6, 3, 7, 4],
+    [
+      [2, 5, 3],
+      [4, 4, 1],
+      [1, 7, 3],
+    ]
+  )
+);
