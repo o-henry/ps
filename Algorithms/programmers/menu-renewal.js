6bfda7ac557dbcdada2123c9431c4fdc61d59001
henry <cksgod16@gmail.com>
2021-08-17 21:06:23 +0900

    Update

---
+++ Algorithms/programmers/menu-renewal.js
@@ -0,0 +1,14 @@
+function solution(o, c) {
+  let answer = [];
+  let tmp = "";
+
+  function DFS(L, s) {}
+
+  DFS(0, 0);
+
+  console.log(answer, tmp);
+}
+
+let orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"];
+let course = [2, 3, 4];
+console.log(solution(orders, course));
