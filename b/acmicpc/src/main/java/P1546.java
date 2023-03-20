d4c366da122109a6630a12f8dd9a3868a319e806
henry <cksgod16@gmail.com>
2023-03-20 22:35:28 +0900

    update

---
+++ b/acmicpc/src/main/java/P1546.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P1546 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int A[] = new int[N];
+        for(int i=0; i<N; i++){
+            A[i] = sc.nextInt();
+        }
+    }
+
+}
---
+++ b/programmers/src/main/java/CH04/P4_2.java
@@ -0,0 +1,5 @@
+package CH04;
+
+public class P4_2 {
+
+}
